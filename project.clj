80603853c9c4b43514fc42c57ff01a6c2ee57209
lgrapenthin <grapenthin.leon@gmail.com>
2015-07-24 19:04:41 +0200

    update clojure

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://github.com/commos/service"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0-RC2"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  
                  #_[org.commos/shared "0.1.0"]
